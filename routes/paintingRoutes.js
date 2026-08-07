bed4725cc95b83e4d2188316239e46362fd702fb
Nada Hashem <Nooody2005std@gmail.com>
2026-08-07 17:27:36 +0300

    painting router done

--- routes/paintingRoutes.js
+++ routes/paintingRoutes.js
@@ -1,69 +1,52 @@
-// const express = require('express');
-// const router = express.Router();
-// const tourController = require('./../controllers/tourController');
-// const authController = require('./../controllers/authController');
-// // const reviewController = require('./../controllers/reviewController');
-// const reviewRouter = require('./../routes/reviewRoutes');
+const express = require('express');
+const router = express.Router();
+const paintingController = require('./../controllers/paintingController');
+const authController = require('./../controllers/authController');
+// const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
-// router.use('/:tourId/reviews', reviewRouter);
+router.use('/:paintingId/reviews', reviewRouter);
 
-// // router
-// //   .route('/:tourId/reviews')
-// //   .post(
-// //     authController.protect,
-// //     authController.restrictTo('user'),
-// //     reviewController.createReview
-// //   );
+// router
+//   .route('/:paintingId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
 
-// // router.param('id', tourController.checkId);
+// router.param('id', paintingController.checkId);
 
 // router
 //   .route('/top-5-cheap')
-//   .get(tourController.aliasTopTour, tourController.getAllTours);
+//   .get(paintingController.aliasTopPainting, paintingController.getAllPaintings);
 
-// router.route('/tour-stats').get(tourController.getTourStats);
 
-// router
-//   .route('/monthly-plan/:year')
-//   .get(
-//     authController.protect,
-//     authController.restrictTo('admin', 'lead-guide','guide'),
-//     tourController.getMonthlyPlan
-//   );
+router
+  .route("/")
+  .get(paintingController.getAllPaintings)
+  .post(
+    authController.protect,
+    authController.restrictTo("admin", "lead-guide"),
+    paintingController.createPainting
+  );
 
-// router.route('/tours-within/:distance/center/:latlng/unit/:unit').get(tourController.getToursWithin);
-// // tours-Within?distance=233&center =- 40,45 & unit=mi 
-// // tours-Within/233/center/-40,45/unit/mi 
+//   .post(paintingController.checkBody, paintingController.createPainting);
 
-// router
-//   .route('/distances/:latlng/unit/:unit')
-//   .get(tourController.getDistances);
+router
+  .route('/:id')
+  .get(paintingController.getPainting)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    paintingController.uploadPaintingImages,
+    paintingController.resizePaintingImages,
+    paintingController.updatePainting
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    paintingController.deletePainting
+  );
 
-// router
-//   .route('/')
-//   .get(tourController.getAllTours)
-//   .post(
-//     authController.protect,
-//     authController.restrictTo('admin', 'lead-guide'),
-//     tourController.createTour
-//   );
-
-// //   .post(tourController.checkBody, tourController.createTour);
-
-// router
-//   .route('/:id')
-//   .get(tourController.getTour)
-//   .patch(
-//     authController.protect,
-//     authController.restrictTo('admin', 'lead-guide'),
-//     tourController.uploadTourImages,
-//     tourController.resizeTourImages,
-//     tourController.updateTour
-//   )
-//   .delete(
-//     authController.protect,
-//     authController.restrictTo('admin', 'lead-guide'),
-//     tourController.deleteTour
-//   );
-
-// module.exports = router;
+module.exports = router;
